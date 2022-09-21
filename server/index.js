b14ba1e397cfb0014f23bab4a83f267d72300d60
Eric Cho <eric6cho.repo@gmail.com>
2022-09-20 21:43:40 -0400

    switched server to prod server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,8 @@ const request = require('request');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8888;
-
-const IMAGEMESHHOST = 'http://localhost:5000'; // test server on local
-//const IMAGEMESHHOST = 'https://image-mesh-server.herokuapp.com'; // prod server on heroku
+//const IMAGEMESHHOST = 'http://localhost:5000'; // test server on local
+const IMAGEMESHHOST = 'https://image-mesh-server.herokuapp.com'; // prod server on heroku
 const IMAGEMESHAPI = IMAGEMESHHOST+'/image-mesh/api';
 const SERVERAPI = '/api';
 
